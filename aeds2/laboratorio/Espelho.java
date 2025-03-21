fd2825514e65a43ae1d9956336ced6f351e4bcc6
Mateus Pimenta de Pádua Brandão <145139540+mateusppb@users.noreply.github.com>
2025-03-21 11:26:58 -0300

    Update Espelho.java

--- aeds2/laboratorio/Espelho.java
+++ aeds2/laboratorio/Espelho.java
@@ -1,3 +1,11 @@
+/*Imprimir números em sequência é uma tarefa relativamente simples.
+Mas e quando se trata de uma sequência espelho? Essa é uma sequência
+que possui um número inicial e um número final, e todos os números entre
+eles, inclusive, são dispostos em ordem crescente, sem espaços, e então essa
+sequência é refletida de forma invertida, como um reflexo no espelho. Por exemplo,
+se a sequência for de 7 a 12, o resultado seria 789101112211101987.
+Escreva um programa que, dados dois números inteiros, imprima a respectiva sequência espelho.*/
+
 import java.util.*;
 
 class Espelho {
